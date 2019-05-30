e354f8e435d2533b71c99b31d76ea0c694aed021
Sergey Zykov <zykau_s@mail.ru>
2019-05-30 13:07:00 +0300

    fix: fix createLine utils function

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,4 +9,10 @@ const getRandomNumber = () => Math.floor(Math.random() * (250 - 50)) + 50
 
 export const getRandomColor = () => `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`
 
-export const createLine = arr => arr.reduce((sum, word) => `${sum}, ${word}`)
+export const createLine = (arr) => {
+  if (!arr.length) {
+    return ''
+  }
+
+  return arr.reduce((sum, word) => `${sum}, ${word}`)
+}
