4dcf82ebdc4ff69d0934fe44d388108c95f266f2
Sergey Zykov <zykau_s@mail.ru>
2019-05-31 16:48:47 +0300

    flasrfjls

--- src/redux/containers/TaskContainer.js
+++ src/redux/containers/TaskContainer.js
@@ -6,6 +6,9 @@ import {} from '../actions'
 const mapStateToProps = state => ({
   tagsList: state.tagsState.tagsList,
   wordsList: state.wordsDataState.wordsList,
+  tagsForTask: state.tasksState.tagsForTask,
+  amountOfWords: state.tasksState.amountOfWords,
+  random: state.tasksState.random,
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch)
